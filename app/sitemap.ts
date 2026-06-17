3699b6f9f5ee22b867010fa93ba0c79025dd5d87
brayan <brayan.challco.ticona2@gmail.com>
2026-06-17 15:39:15 -0400

    Agregar sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,50 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://teoria-colas-xgig.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://teoria-colas-xgig.vercel.app/teoria-colas",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://teoria-colas-xgig.vercel.app/teoria-colas/mm1",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://teoria-colas-xgig.vercel.app/teoria-colas/mms",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://teoria-colas-xgig.vercel.app/teoria-colas/mmic",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://teoria-colas-xgig.vercel.app/teoria-colas/mmsc",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://teoria-colas-xgig.vercel.app/teoria-colas/mmsk",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://teoria-colas-xgig.vercel.app/teoria-colas/mmik",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://teoria-colas-xgig.vercel.app/contacto",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://teoria-colas-xgig.vercel.app/acerca-de",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://teoria-colas-xgig.vercel.app/privacidad",
+      lastModified: new Date(),
+    },
+  ];
+}
